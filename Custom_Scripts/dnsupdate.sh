0073dce1531e498a330aea7e47b342d763d9f7df
Jay K <j4y-k@users.noreply.github.com>
2017-08-09 15:10:42 -0400

    updated bash script

--- Custom_Scripts/dnsupdate.sh
+++ Custom_Scripts/dnsupdate.sh
@@ -1,16 +1,25 @@
-﻿#!/bin/sh
+#!/bin/sh
+
+# Azure - Register Linux VM against DC
+# By Jay Klesitz
+# Run script on VM: 
+# chmod 755 /etc/dhcp/dnsupdate.sh ; /etc/dhcp/dnsupdate.sh ; chmod 755 /etc/rc.d/rc.local; echo "/etc/dhcp/dnsupdate.sh" >> /etc/rc.d/rc.local ; reboot
+
 
 DCSERVER="server poc-eus-dc1.iglooaz.local"
 ZONE="zone iglooaz.local"
 ADDR=`/sbin/ifconfig eth0 | grep 'inet ' | awk '{print $2}' | sed -e s/.*://`
-HOST=`hostname -f`
+HOST=`hostname`
 DOMAIN=".iglooaz.local."
+RESOLVEDOMAIN="iglooaz.local"
 
-echo "Updating DNS"
-echo "$DCSERVER" > /etc/dhcp/dnsupdate.txt
-echo "$ZONE" >> /etc/dhcp/dnsupdate.txt
+
+echo $DCSERVER > /etc/dhcp/dnsupdate.txt
+echo $ZONE >> /etc/dhcp/dnsupdate.txt
 echo "update delete $HOST$DOMAIN A" >> /etc/dhcp/dnsupdate.txt
 echo "update add $HOST$DOMAIN 86400 A $ADDR" >> /etc/dhcp/dnsupdate.txt
 echo "show" >> /etc/dhcp/dnsupdate.txt
 echo "send" >> /etc/dhcp/dnsupdate.txt
 nsupdate /etc/dhcp/dnsupdate.txt
+
+echo "domain" $RESOLVEDOMAIN >> /etc/resolv.conf
